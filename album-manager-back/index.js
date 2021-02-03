debe38faccc9f37c788898fc1cf9977c78c0f0a8
Florian-Pesset <florianpesset@gmail.com>
2021-02-03 13:59:37 +0100

    config backend

---
+++ album-manager-back/index.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const cors = require("cors");
+const connection = require("./config");
+const port = 8080;
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my albums manager");
+});
+
+app.get("/albums", (req, res) => {
+  connection.query("SELECT * FROM album", (err, results) => {
+    if(err) {
+      res.setStatus(500);
+    } else if (results.length === 0) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(results);
+    }
+  })
+});
+
+//as a user, I want to be able to create a new album.
+app.post("/albums", (req, res) => {
+  const { title, genre, picture, artist } = req.body;
+  connection.query(
+    "INSERT INTO album(title, genre, picture, artist) VALUES(?, ?, ?, ?)",
+    [title, genre, picture, artist], 
+    (err, results) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            res.status(201).json({...req.body, id: results.insertId});
+          }
+        }
+    ); 
+});
+
+//as a user, I want to be able to see an album by entering its id in the url.
+app.get("/albums/:id", (req, res) => {
+  connection.query(
+    `SELECT * FROM album WHERE id=?`,
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else if (results.length === 0) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).json(results[0]);
+      }
+    }
+  );
+});
+
+//as a user, I want to create and assign a song to an album.
+app.post("/albums/:id/tracks", (req, res) => {
+  const { title, youtube_url } = req.body;
+  const id_album = req.params.id;
+  connection.query(
+    `INSERT INTO track(title, youtube_url, id_album) VALUES (?, ?, ?)`,
+    [title, youtube_url, id_album],
+    (err, results) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            res.status(201).json({...req.body, id: results.insertId});
+          }
+        }
+    ); 
+});
+
+//as a user, I want to list all the songs from an album.
+app.get("/albums/:id/tracks", (req, res) => {
+  const id_album = req.params.id;
+  connection.query(
+    `SELECT * FROM track WHERE id_album=?`,
+    [id_album],
+     (err, results) => {
+    if(err) {
+      res.setStatus(500);
+    } else if (results.length === 0) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(results);
+    }
+  })
+});
+
+//as a user, I want to be able to delete an album.
+app.delete("/albums/:id", (req, res) => {
+  const idAlbum = req.params.id;
+  connection.query(
+    "DELETE FROM album WHERE id = ?",
+    [idAlbum],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(204).send("Album deleted!");
+      }
+    }
+  );
+});
+
+//as a user, I want to be able to modify an album.
+app.put("/albums/:id", (req, res) => {
+  const idAlbum = req.params.id;
+  const newAlbum = req.body;
+  connection.query(
+    "UPDATE album SET ? WHERE id = ?",
+    [newAlbum, idAlbum],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).json({...req.body});
+      }
+    }
+  );
+});
+
+//as a user, I want to delete a song.
+app.delete("/albums/:id_album/tracks/:id", (req, res) => {
+  const id_album = req.params.id_album;
+  const id = req.params.id;
+  connection.query(
+    "DELETE FROM track WHERE id_album = ? AND id = ?",
+    [id_album, id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(204).send("Song deleted!");
+      }
+    }
+  );
+});
+
+//as a user, I want to edit a song from an album.
+app.put("/albums/:id_album/tracks/:id", (req, res) => {
+  const id_album = req.params.id_album;
+  const id = req.params.id;
+  const newTrack = req.body;
+  connection.query(
+    "UPDATE track SET ? WHERE id_album = ? AND id = ?",
+    [newTrack, id_album, id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).json({...req.body});
+      }
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server is runing on ${port}`);
+});
