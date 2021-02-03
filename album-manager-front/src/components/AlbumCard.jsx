9a38439990b1eee8605700432cb79f4b920bc06b
Florian-Pesset <florianpesset@gmail.com>
2021-02-03 13:42:51 +0100

    css card

--- album-manager-front/src/components/AlbumCard.jsx
+++ album-manager-front/src/components/AlbumCard.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../css/albumCard.module.css";
 
 export default function AlbumCard({ albumcard }) {
   return (
-    <div>
-      <img src={albumcard.image} alt={albumcard.title} />
+    <div className={styles.card}>
+      <Link to={`/${albumcard.id}`}>
+        <img
+          src={albumcard.image}
+          alt={albumcard.title}
+          className={styles.image}
+        />
+      </Link>
       <p>
         {albumcard.artist} / {albumcard.title}
       </p>
